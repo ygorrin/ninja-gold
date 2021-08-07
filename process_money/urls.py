747401c52c592cdd42397efc2acb1f3a4f9f72dc
Yamy <yamilagorrin@gmail.com>
2021-08-07 08:04:19 -0400

    Ninja Gold V2

--- process_money/urls.py
+++ process_money/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.index),
-    path('process_money', views.process_money),
-    #path('second/<name>', views.second)
+    path('process_money/', views.index),
+    path('crear_datos/', views.crear_datos),
+    path('vaciar_datos/', views.vaciar_datos),
 ]
--- process_money/views.py
+++ process_money/views.py
@@ -2,27 +2,40 @@ from django.shortcuts import render, HttpResponse, redirect
 import random
 from time import localtime,strftime
 
-texto = []
-def index(request):
-    if 'contador' in request.session:
-        pass
-    else:
-        request.session['contador'] = 0
-
-    context = {
-        "contador": request.session['contador'],
-    }
-    return render(request,'index.html', context)
-
+# Funcion random
 def randInt(min=0,max=100):
     num = (random.random() * (max-min)+min)
     return round(num)
 
+#Funcion tiempo
 def gettime():
     time =strftime("%b %d, %Y, %H:%M %p", localtime())
     return time
 
-def process_money(request):
+
+#Funcion index
+def index(request):
+    print("Ejecutado index")
+    return render(request,'index.html')
+
+
+#Función principal métodos post
+def crear_datos(request):
+    #Si no existen, lo creara
+    if 'contador' not in request.session:
+        request.session['contador'] = 0
+    if 'contador1' not in request.session:
+        request.session['contador1'] = 0
+    else:
+        request.session['contador1'] += 1
+    if 'logs' not in request.session:
+        request.session['logs'] = [] 
+    color = ""
+    textoadd = ""  
+    texto = []  
+    # a esta altura existen contadores y variables. 
+    print("Ejecutado crear datos - Creacion variables")
+
     if request.POST:
         print("opcion:", request.POST['opcion'])
         opcion = request.POST['opcion']
@@ -34,21 +47,36 @@ def process_money(request):
             numero = randInt(2,5)
         elif opcion =="casino":
             numero = randInt(-50,50)
+        request.session['contador'] = request.session['contador'] + numero
+        
+        if numero>=0:
+            color = "azul"
+            textoadd = "Ganaste: " + str(numero) + " Gold desde " + opcion + "!  (" + gettime() + ")"
+        else: 
+            color = "rojo"
+            textoadd = "Perdiste: " + str(numero) + " Gold desde " + opcion + " Sorry!  (" + gettime() + ")"
+        print("Ejecutado crear datos - Post")
 
-    print("Valor Inicial: ",request.session['contador'])
-    request.session['contador'] += numero
-    if numero>=0:
-        color = "blue"
-        textoadd = "Ganaste: " + str(numero) + " Gold desde la " + opcion + "!  (" + gettime() + ")"
-    else: 
-        color = "red"
-        textoadd = "Perdiste: " + str(numero) + " Gold desde la " + opcion + " Sorry!  (" + gettime() + ")"
-    print("Random: ", numero , "Request: ", request.session['contador'], textoadd)
-    texto.append(textoadd)
+    datos_a_entregar = {
+        'contador' : request.session['contador'], #Cuenta las monedas por jugada
+        'contador1': request.session['contador1'], #Cuental el número de la jugada
+        'color': color,
+        'texto' : textoadd,
+        }
 
-    context = {
-        'contador':  request.session['contador'],
-        'texto': texto,
-        'color' : color,
-    }
-    return render(request, 'index.html', context)
+    request.session['logs'].append(datos_a_entregar)
+    request.session.save()
+    print("Ejecutado crear datos")
+    return redirect('/')
+
+#Función para vaciar datos
+def vaciar_datos(request):
+    if 'contador' in request.session:
+        del request.session['contador']
+    if 'contador1' in request.session:
+        del request.session['contador1']
+    if 'logs' in request.session:
+        del request.session['logs']
+    crear_datos(request)
+    print("Ejecutado vaciar datos")
+    return redirect('/')
